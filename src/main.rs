a04255c7fb2969ebc8cbd90451b69d1fc5210789
Castle96 <kcastle96@gmail.com>
2025-08-05 16:08:25 -0500

    added test runner and qemu support

--- src/main.rs
+++ src/main.rs
@@ -73,3 +73,32 @@ fn trivial_assertion() {
     assert_eq!(1, 1);
     println!("[ok]");
 }
+
+// in src/main.rs
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+// in src/main.rs
+
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    /// new
+    exit_qemu(QemuExitCode::Success);
+}
