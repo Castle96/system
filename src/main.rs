cea81224e822cce2dc450e366c3ef69f75cc23f5
Castle96 <kcastle96@gmail.com>
2025-08-05 15:51:05 -0500

    updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -36,3 +36,40 @@ pub extern "C" fn _start() -> ! {
 
     loop {}
 }
+
+
+// in src/main.rs
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+// in src/main.rs
+
+#![reexport_test_harness_main = "test_main"]
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+// in src/main.rs
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
